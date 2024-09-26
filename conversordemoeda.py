be473984383c9f6fced3abf23da980332e42ff1d
williams da silva <tjswilliams2016@gmail.com>
2024-09-25 21:56:58 -0300

    Seu commit

---
+++ conversordemoeda.py
@@ -0,0 +1,14 @@
+n1=float(input('quanto voce tem na carteira R$? '))
+
+taxa= 3.27
+conversor= n1/taxa
+
+print('voce com R${:.2f} consegue converter para USD ${:.2f}'.format(n1, conversor))
+
+n2= float(input('quanto voce tem em USD? '))
+
+dolar=5.57
+conver=n2*dolar
+
+print('voce com USD {:.2f} consegue converter R${:.2f}'.format(n1, conver))n
+
